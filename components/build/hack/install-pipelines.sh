501f7d2bfc6cfef3c83eb70638c6848b5605c77b
John Duimovich <jduimovich@gmail.com>
2021-11-21 19:12:52 -0500

    default pipeline and build scripts

--- components/build/hack/install-pipelines.sh
+++ components/build/hack/install-pipelines.sh
@@ -5,9 +5,45 @@ PROJECT=$(oc config view --minify -o 'jsonpath={..namespace}')
 
 PATCH_NS="$(printf '.items[].metadata.namespace="%q"' $PROJECT)" 
 
-# Fetch all default build pipelines and install into current namespace 
-#oc get pipelines -n build-templates -o yaml  | yq  e "$PATCH_NS" - 
-oc get pipelines -n build-templates -o yaml  | yq  e "$PATCH_NS" - | oc apply -f -
-#oc get tasks -n build-templates -o yaml  | yq  e "$PATCH_NS" - 
-oc get tasks -n build-templates -o yaml  | yq  e "$PATCH_NS" - | oc apply -f -
-oc get pvc -n build-templates -o yaml  | yq  e "$PATCH_NS" - | oc apply -f -
+declare -a totrim=(
+        "metadata.managedFields"  
+        "metadata.creationTimestamp"  
+        "metadata.generation"   
+        "metadata.annotations"  
+        "metadata.resourceVersion"  
+        "metadata.uid"   
+         ) 
+
+echo installing default pipelines in $PROJECT
+
+oc get pipelines -n build-templates -o yaml > pipelines.yaml 
+oc get pvc -n build-templates -o yaml > pvc.yaml
+yq eval 'del(.items[].metadata.labels["app.kubernetes.io/instance"])' pipelines.yaml -i 
+yq eval 'del(.items[].metadata.labels["app.kubernetes.io/instance"])' pvc.yaml -i
+
+yq eval 'del(.items[].metadata.labels | select(length==0))' pipelines.yaml -i 
+
+## Delete unneeded fields 
+
+for i in "${totrim[@]}"
+do 
+    PATCH_FIELD="$(printf 'del(.items[].%q)' $i)" 
+    #echo "$PATCH_FIELD"  
+    yq eval "$PATCH_FIELD"  pipelines.yaml -i  
+    yq eval "$PATCH_FIELD"  pvc.yaml -i 
+done 
+
+declare -a patchs=(
+        "pipelines.yaml"   
+        "pvc.yaml"    
+         ) 
+for i in "${patchs[@]}"
+do 
+yq  e "$PATCH_NS" $i  -i  
+oc apply -f  $i
+#for debug comment out
+rm $i 
+done     
+oc get pipelines 
+
+ 
